1fe31339ea49700319e467a4f1d2fd5f137f2cb7
k-gnusarev <kgnusaryov@yandex.ru>
2021-03-31 14:31:53 +0400

    feat: реализовать меню регистрации

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,10 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Register(props) {
-  return (
-    <div className>
-      Тут будет регистрация!
-    </div>
+  return (    
+    <section className="start-section">
+      <h1 className="start-section__heading">Регистрация</h1>
+      <form className="start-section__form">
+        <input
+          className="start-section__input"
+          name="loginEmail"
+          type="email"
+          placeholder="Email"
+          id="login-email"
+          required></input>
+        <input
+          className="start-section__input"
+          name="loginPassword"
+          type="password"
+          placeholder="Пароль"
+          id="login-password"
+          required></input>
+        <button className="button button_style_white">Зарегистрироваться</button>
+      </form>
+      <p className="start-section__text">
+        Уже зарегистрированы? <Link to="/sign-in" className="start-section__link">Войти</Link>
+      </p>
+    </section>
   );
 }
 
